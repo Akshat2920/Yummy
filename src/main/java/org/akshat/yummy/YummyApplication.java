e97a963271dfadd40613e08da12cce8dba4b788a
akshat2920 <akshatlal211@gmail.com>
2024-11-02 08:21:46 +0530

    ..

--- src/main/java/org/akshat/yummy/YummyApplication.java
+++ src/main/java/org/akshat/yummy/YummyApplication.java
@@ -2,10 +2,10 @@ package org.akshat.yummy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class YummyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(YummyApplication.class, args);
     }
